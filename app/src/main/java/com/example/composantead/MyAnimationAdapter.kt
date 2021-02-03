1564ce1a58431afc360cf06d02acfd351d6e3c51
BOIRO <mamadou-yero.boiro@capgemini.com>
2021-02-03 09:39:39 +0100

    Projet Prof + Animators in gradle

---
+++ app/src/main/java/com/example/composantead/MyAnimationAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.composantead
+
+//package jp.wasabeef.example.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
+
+class MyAnimatorAdapter constructor(
+    adapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>,
+    from: Float = 0.5f
+) : AlphaInAnimationAdapter(adapter, from) {
+}
---
+++ app/src/main/java/com/example/composantead/MyAnimator.kt
@@ -0,0 +1,11 @@
+package com.example.composantead
+//package jp.wasabeef.example.recyclerview
+
+import android.view.animation.Interpolator
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
+import jp.wasabeef.recyclerview.animators.ScaleInRightAnimator
+
+class MyAnimator(
+    interpolator: Interpolator = LinearOutSlowInInterpolator()
+) : ScaleInRightAnimator(interpolator) {
+}
